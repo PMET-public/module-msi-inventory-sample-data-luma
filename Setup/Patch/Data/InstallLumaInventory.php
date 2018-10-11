08f79711ad360e06066e39407eeaac9bcc4476d5
Jeff Britts <jbritts@adobe.com>
2018-10-11 11:36:14 -0400

    moved install to Magneto_MsiInventorySampleData

--- Setup/Patch/Data/InstallLumaInventory.php
+++ Setup/Patch/Data/InstallLumaInventory.php
@@ -6,14 +6,10 @@
 
 namespace Magento\MsiLumaInventorySampleData\Setup\Patch\Data;
 
-use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
-use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
-use Magento\InventoryApi\Api\Data\SourceItemInterface;
-use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
-use Magento\InventoryApi\Api\SourceItemsDeleteInterface;
+use Magento\MsiInventorySampleData\Model\InstallInventoryData as SampleData;
 
 
 /**
@@ -31,83 +27,30 @@ class InstallLumaInventory implements DataPatchInterface
      */
     protected $fixtureManager;
 
-    /** @var SearchCriteriaBuilder  */
-    protected $searchCriteriaBuilder;
+    /** @var SampleData\  */
+    protected $sampleData;
 
-    /** @var SourceItemRepositoryInterface  */
-    protected $sourceItemRepository;
-
-    /** @var $sourceItemInterfaceFactory */
-    protected $sourceItemInterface;
-
-    /** @var SourceItemsDeleteInterface  */
-    protected $sourceItemsDelete;
-
+    /**
+     * InstallLumaInventory constructor.
+     * @param SampleDataContext $sampleDataContext
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param SampleData $sampleData
+     */
     public function __construct(
         SampleDataContext $sampleDataContext,
         ModuleDataSetupInterface $moduleDataSetup,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        SourceItemRepositoryInterface $sourceItemRepository,
-        SourceItemInterfaceFactory $sourceItemInterfaceFactory,
-        SourceItemsDeleteInterface $sourceItemsDelete
+        SampleData $sampleData
        )
     {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->fixtureManager = $sampleDataContext->getFixtureManager();
-        $this->csvReader = $sampleDataContext->getCsvReader();
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->sourceItemRepository = $sourceItemRepository;
-        $this->sourceItemInterface = $sourceItemInterfaceFactory;
-        $this->sourceItemsDelete = $sourceItemsDelete;
+        $this->sampleData = $sampleData;
     }
 
 
     public function apply()
     {
-        $this->addInventory(['Magento_MsiLumaInventorySampleData::fixtures/luma_msi_inventory.csv']);
-    }
-
-    public function addInventory(array $fixtures): void
-    {
-        foreach ($fixtures as $fileName) {
-            $fileName = $this->fixtureManager->getFixture($fileName);
-            if (!file_exists($fileName)) {
-                continue;
-            }
-
-            $rows = $this->csvReader->getData($fileName);
-            $header = array_shift($rows);
-
-            foreach ($rows as $row) {
-                $data = [];
-                foreach ($row as $key => $value) {
-                    $data[$header[$key]] = $value;
-                }
-                //if sku/source has already been defined, skip
-                $search = $this->searchCriteriaBuilder
-                    ->addFilter(SourceItemInterface::SOURCE_CODE,$data['source_code'],'eq')
-                    ->addFilter(SourceItemInterface::SKU, $data['sku'], 'eq')
-                    ->create();
-                $sourceItemSearch = $this->sourceItemRepository->getList($search)->getTotalCount();
-                if($sourceItemSearch==0) {
-                    $sourceItem = $this->sourceItemInterface->create();
-                    $sourceItem->setSku($data['sku']);
-                    $sourceItem->setSourceCode($data['source_code']);
-                    $sourceItem->setQuantity($data['quantity']);
-                    $sourceItem->setStatus($data['status']);
-                    $sourceItem->save();
-                }
-                //remove default inventory
-                $search = $this->searchCriteriaBuilder
-                    ->addFilter(SourceItemInterface::SOURCE_CODE,'default','eq')
-                    ->addFilter(SourceItemInterface::SKU, $data['sku'], 'eq')
-                    ->create();
-                $defaultItems = $this->sourceItemRepository->getList($search)->getItems();
-                if(count($defaultItems)){
-                    $this->sourceItemsDelete->execute($defaultItems);
-                }
-            }
-        }
+        $this->sampleData->addInventory(['Magento_MsiLumaInventorySampleData::fixtures/luma_msi_inventory.csv']);
     }
 
     /**
